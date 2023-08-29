c1bbaeab6053c1576a722355ef3a2856b787f153
SCenturionZ <sebastian.ignacio.centurion@gmail.com>
2023-08-29 03:56:28 -0300

    Add Components View Contacto

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,10 +1,29 @@
-import { React } from 'react';
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import ContactoInstitucion from '../ContactoInstitucion/ContactoInstitucion';
+import ContactoTecnico from '../ContactoTecnico/ContactoTecnico';
+import ContactoForm from '../ContactoForm/ContactoForm';
+
+import './Contacto.css';
 
 function Contacto() {
     return (
-        <>
-            <h1>Contacto 📧</h1>
-        </>);
+        <div className="container mt-5 justify-content-md-center">
+            <Row className="row justify-content-between w-100">
+                <Col md={5} xs="12">
+                    <ContactoInstitucion/>
+                </Col>
+                <div className="col d-none d-md-block">
+                    <hr className="vertical" />
+                </div>
+                <Col md={5} xs="12">
+                    <ContactoTecnico/>
+                    <ContactoForm/>
+                </Col>
+            </Row>
+        </div>
+        );
 }
 
 export default Contacto;
